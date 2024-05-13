42a191e1d4604dae8fa039de78c9f80fa23da3c9
bae080311 <156505831+bae080311@users.noreply.github.com>
2024-05-13 09:33:32 +0900

    fix: title

---
+++ potato.js
@@ -0,0 +1,11 @@
+const h1 = document.querySelector("div.hello:first-child h1");
+
+
+function handletitleclick() {
+     if (h1.style.color === "blue"){ 
+          h1.style.color = "tomato";
+     }
+     else 
+     h1.style.color = "blue"
+}
+h1.addEventListener("click", handletitleclick);
